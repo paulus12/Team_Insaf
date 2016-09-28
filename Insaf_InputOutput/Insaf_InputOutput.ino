11d3d632afef1d8c6c9854e7596b5f0aa2ff292d
paulus12 <paulusppn@gmail.com>
2016-09-28 15:51:52 +0700

    Add files via upload

---
+++ Insaf_InputOutput/Insaf_InputOutput.ino
@@ -0,0 +1,16 @@
+
+int ledPin = 2; //led di pin no 2
+int inPin = 7;  //tombol di pin no 7
+int val = 0;    //variabel menyimpan nilai yg terbaca
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(ledPin, OUTPUT);    //digital pin 13 sbgai output
+pinMode(inPin, INPUT);      //digital pin 7 sbgai input
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val = digitalRead(inPin);   //baca pin input
+  digitalWrite(ledPin, val);   //mengatur LED ke nilai tombol
+}
---
+++ Insaf_LED/Insaf_LED.ino
@@ -0,0 +1,14 @@
+//TestInsaf1_Blink LED
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(2,OUTPUT); //Digital pin 12 sebagai output
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(2,HIGH); //LED on
+  delay(1000);            //1 detik menunggu
+  digitalWrite(2,LOW);  //LED mati
+  delay(2000);            //2 detik menunggu
+}
