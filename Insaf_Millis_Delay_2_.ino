99f45f7ae0a55a0740dd4c976b230011556a4cf2
Paulus Lumbansiantar <paulusppn@gmail.com>
2016-10-10 07:25:35 +0700

    Add files via upload

---
+++ Insaf_Millis_Delay_2_.ino
@@ -0,0 +1,29 @@
+const int pinnyaLED =13;
+int kondisiLED = LOW;
+long MillisSebelumnya = 0;
+long intervalWaktu = 1000;
+
+void setup() 
+{
+  // put your setup code here, to run once:
+pinMode(pinnyaLED,OUTPUT);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+unsigned long MillisSaatIni = millis();
+if(MillisSaatIni - MillisSebelumnya > intervalWaktu)
+{
+  MillisSebelumnya = MillisSaatIni;
+if(kondisiLED = LOW)
+{
+  kondisiLED = HIGH;
+  }
+else
+{
+  kondisiLED = LOW;
+  }
+  digitalWrite(pinnyaLED,kondisiLED);
+  }
+}
---
+++ Insaf_PotentiometerwithLED.ino
@@ -0,0 +1,25 @@
+//untuk penggunaan dua lampu :)
+int LED = 6; // digital pin untuk LED
+int led = 9; // digital pin untuk LED 
+int lEd = 11; // digital pin untuk LED
+int pinPot = 0; // pin analog untuk VR
+int potVal = 0; // tempat menerima nilai tegangan dari VR
+
+void setup() {
+pinMode(LED,OUTPUT); // LED sebagai outputnya
+pinMode(led,OUTPUT); // led sebagai output
+pinMode(lEd,OUTPUT); // lEd sebagai output
+}
+
+void loop() {
+potVal = analogRead(pinPot); //menerima masukan nilai dari VR;
+potVal = map(potVal, 0, 1023, 0 , 255); //ubah nilai (0-1023) jadi(0-255) 
+//map untuk apa?
+analogWrite(LED,potVal); // ubah nilai VR untuk mengatur kecerahan
+analogWrite(led,potVal); // ubah nilai VR untuk mengatur kecerahan
+analogWrite(lEd,potVal); // ubah nilai VR untuk mengatur kecerahan
+}
+/*jika diubah selain digital pin 5,6,9,10,11, maka nyala lampu tidak redup :D :V, 
+kenapa bisa seperti ini?
+digital pin 1,2,3,4,7,8,12,13 
+*/
